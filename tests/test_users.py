f03d5f634289b6351461e2abbb5846fce0ea8596
lrayne <legaterayne@gmail.com>
2025-11-06 00:53:43 +0300

    Add tests for pagination

--- tests/test_users.py
+++ tests/test_users.py
@@ -55,3 +55,98 @@ class TestShowNonExistentUsers:
         response = requests.get(f"{app_url}/api/users/{user_id}")
 
         assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.usefixtures("prepare_users_data")
+class TestUsersPagination:
+    def test_check_users_total(self, app_url: str):
+        response = requests.get(f"{app_url}/api/users")
+
+        result = response.json()
+
+        assert response.status_code == HTTPStatus.OK
+        assert result["page"] == 1
+        assert result["size"] == 10
+        assert len(result["items"]) == 10
+        assert result["total"] == 50
+
+    def test_paginate_users(self, app_url: str):
+        response = requests.get(
+            f"{app_url}/api/users",
+            params={"page": 2},
+        )
+
+        result = response.json()
+
+        assert response.status_code == HTTPStatus.OK
+        assert result["page"] == 2
+        assert result["size"] == 10
+        assert len(result["items"]) == 10
+        assert result["total"] == 50
+
+    def test_change_users_size_per_page(self, app_url: str):
+        response = requests.get(
+            f"{app_url}/api/users",
+            params={"size": 2},
+        )
+
+        result = response.json()
+
+        assert response.status_code == HTTPStatus.OK
+        assert result["page"] == 1
+        assert result["size"] == 2
+        assert len(result["items"]) == 2
+        assert result["total"] == 50
+
+    def test_check_invalid_users_page_min_boundary(self, app_url: str):
+        response = requests.get(
+            f"{app_url}/api/users",
+            params={"page": 0},
+        )
+
+        result = response.json()
+
+        assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+        assert (
+            result["detail"][0]["msg"]
+            == "Input should be greater than or equal to 1"
+        )
+
+    def test_check_invalid_users_page_max_boundary(self, app_url: str):
+        response = requests.get(
+            f"{app_url}/api/users",
+            params={"page": 6},
+        )
+
+        result = response.json()
+
+        assert response.status_code == HTTPStatus.OK
+        assert len(result["items"]) == 0
+
+    def test_check_invalid_users_page_min_boundary_size(self, app_url: str):
+        response = requests.get(
+            f"{app_url}/api/users",
+            params={"size": 0},
+        )
+
+        result = response.json()
+
+        assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+        assert (
+            result["detail"][0]["msg"]
+            == "Input should be greater than or equal to 1"
+        )
+
+    def test_check_invalid_users_page_max_boundary_size(self, app_url: str):
+        response = requests.get(
+            f"{app_url}/api/users",
+            params={"size": 21},
+        )
+
+        result = response.json()
+
+        assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+        assert (
+            result["detail"][0]["msg"]
+            == "Input should be less than or equal to 20"
+        )
