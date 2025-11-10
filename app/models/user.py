7a065a0bc62e4dd84333a5c4d393faa05545db18
lrayne <legaterayne@gmail.com>
2025-11-10 23:45:13 +0300

    Implement users CRUD

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from pydantic import EmailStr
+from pydantic import BaseModel, EmailStr, HttpUrl
 from sqlmodel import Field, SQLModel
 
 
@@ -8,3 +8,17 @@ class User(SQLModel, table=True):
     first_name: str
     last_name: str
     avatar: str
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    first_name: str
+    last_name: str
+    avatar: HttpUrl
+
+
+class UserUpdate(BaseModel):
+    email: EmailStr | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    avatar: HttpUrl | None = None
