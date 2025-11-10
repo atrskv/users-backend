bc577a1fd82b26ef5a701272f9adce4dd7555c9c
lrayne <legaterayne@gmail.com>
2025-11-10 23:45:13 +0300

    Implement users CRUD

--- app/db/users.py
+++ app/db/users.py
@@ -1,5 +1,6 @@
 from collections.abc import Iterable
 
+from fastapi import HTTPException
 from sqlmodel import Session, select
 
 from app.db.engine import engine
@@ -14,4 +15,38 @@ def get_user(user_id: int) -> User | None:
 def get_users() -> Iterable[User] | None:
     with Session(engine) as s:
         statement = select(User)
+
         return s.exec(statement).all()
+
+
+def create_user(user: User) -> User:
+    with Session(engine) as s:
+        s.add(user)
+        s.commit()
+        s.refresh(user)
+
+        return user
+
+
+def update_user(user_id: int, user: User) -> User | None:
+    with Session(engine) as session:
+        db_user = session.get(User, user_id)
+
+        if not db_user:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        user_data = user.model_dump(exclude_unset=True)
+        _ = db_user.sqlmodel_update(user_data)
+
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+        return db_user
+
+
+def delete_user(user_id: int):
+    with Session(engine) as s:
+        user = s.get(User, user_id)
+        s.delete(user)
+        s.commit()
--- app/routers/users.py
+++ app/routers/users.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi_pagination import Page, Params, paginate
 
 from app.db import users
-from app.models.user import User
+from app.models.user import User, UserCreate, UserUpdate
 from app.utils import get_pagination_params
 
 router = APIRouter(prefix="/api/users")
@@ -53,3 +53,31 @@ def clear_users():
     users_list.clear()
 
     return users_list
+
+
+@router.post("/", status_code=HTTPStatus.CREATED)
+def create_user(user: User) -> User:
+    UserCreate.model_validate(user.model_dump())
+    return users.create_user(user)
+
+
+@router.patch("/{user_id}", status_code=HTTPStatus.OK)
+def update_user(user_id: int, user: User) -> User:
+    if user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+            detail="Invalid user id",
+        )
+    UserUpdate.model_validate(user.model_dump())
+    return users.update_user(user_id, user)
+
+
+@router.delete("/{user_id}", status_code=HTTPStatus.OK)
+def delete_user(user_id: int):
+    if user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+            detail="Invalid user id",
+        )
+    users.delete_user(user_id)
+    return {"message": "User deleted"}
