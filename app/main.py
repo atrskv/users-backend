43a3a0bb71d30f380212aac203452f4d53d0afce
lrayne <legaterayne@gmail.com>
2025-11-19 00:16:53 +0300

    Move init db to lifespan

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 import dotenv
 
 dotenv.load_dotenv()
@@ -8,12 +10,19 @@ from fastapi import FastAPI
 from app.db.engine import init_db
 from app.routers import status, users
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(status.router)
 app.include_router(users.router)
 
 
 if __name__ == "__main__":
-    init_db()
     uvicorn.run(app, host="localhost", port=8002)
